bef1c7cac5079a6ae2f3099730b95e8f8e9cb3a2
Gideon Abe <gideonabe2020@gmail.com>
2026-05-01 18:53:49 +0100

    minor update

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -8,7 +8,7 @@ export default function SplashScreen({ onEnter }: { onEnter: () => void }) {
       initial={{ opacity: 1 }}
       exit={{ opacity: 0, scale: 1.1 }}
       transition={{ duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] }}
-      className="fixed inset-0 z-200 h-[100dvh] w-full overflow-hidden"
+      className="fixed inset-0 z-200 h-[100svh] w-full overflow-hidden"
     >
       {/* Background Image - Reference the style in thehemmes.jpeg */}
       <div 
